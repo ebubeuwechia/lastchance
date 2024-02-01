91199646c07b5a8d43ffc6ea98cf7f1763dcf046
Uwechia, Ebubechukwu <euwechia3@gatech.edu>
2024-01-31 19:58:08 -0500

    ui

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,9 +4,15 @@ import { StatusBar } from "expo-status-bar";
 import { Image } from "react-native";
 import { theme } from "../theme";
 import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import {MapPinIcon } from 'react-native-heroicons/solid';
 export default function HomeScreen() {
    const [showSearch, toggleSearch ] = useState(false);
    const [locations, setLocation]  = useState([1,2,3]); //three results to appear
+
+   const handleLocation = (loc) => {
+      console.log('location:', loc);
+   }
+
    return (
     <View classname = "flex-1 re" >
       <StatusBar style = "light" />
@@ -14,17 +20,17 @@ export default function HomeScreen() {
       style = {{height: '100%',  width: "100%"}}> 
 
       <SafeAreaView classname="flex flex-1">
-         <Text style= {{display: 'flex',fontSize:'30%', textAlign: 'center', top: 100}}>
+         <Text style= {{display: 'flex',fontSize:'30%', textAlign: 'center', top: 50}}>
              Welcome to Check Weather!
          </Text>
-         <Text style= {{display: 'flex',fontSize:'20%', textAlign: 'center', top: 150}}>
+         <Text style= {{display: 'flex',fontSize:'20%', textAlign: 'center', top: 100}}>
              Enter the city below to get started</Text>
          {/*search section*/}
          <View style = {{ height: '70%', position:'relative',marginHorizontal:16,zIndex:50 }} classname = "mx-4 relative z-50">
             <View classname="flex-row justify-end items-center rounded-full"
             style={{backgroundColor: showSearch? theme.bgPink(0.5): '.05', height:'1%', paddingLeft: '1.5%', flex:.1, borderWidth: '5', 
             flexDirection: 'row', justifyContent: 'center', alignItems: 'center', borderRadius: 50,
-              top: 250}}>
+              top: 125}}>
                {
                   showSearch? (
                   <TextInput placeholder="Enter city here" style={{paddingLeft: 24, height: 40, flex: 1, fontSize: 16,}}
@@ -42,12 +48,18 @@ export default function HomeScreen() {
             {
                locations.length>0 && showSearch? (
                   <View style={{ position: "absolute", width:'100%', 
-                  backgroundColor:"#C8A2C8", top:300, borderRadius:20, padding:16, paddingTop:16, flex:1}}>
+                  backgroundColor:"#C8A2C8", top:177, borderRadius:20, padding:16, paddingTop:16, flex:1}}>
                      {
                         locations.map((loc,index)=> {
+                           let showBorder = index+1 != locations.length;
                            return (
-                              <TouchableOpacity>
-                                 <Text>London</Text>
+                              <TouchableOpacity
+                              onPress={() => handleLocation(loc)}
+                               key={index} 
+                              style={{flex: 1, justifyContent: 'center', alignItems: 'center',flexDirection: 'row', padding:12,
+                              paddingHorizontal: 16, marginBottom: 4, borderBottomColor: "black", borderBottomWidth:2, width:"100%"}}>
+                                 <MapPinIcon size='20' color= "teal"></MapPinIcon>
+                                 <Text>HTOWN DOWNNN</Text>
                               </TouchableOpacity>
                            )
                         })
@@ -55,6 +67,26 @@ export default function HomeScreen() {
                   </View>
                ):null
             }
+             {/*forecast section*/}
+            <View style={{flex: 1,marginHorizontal: 16, marginBottom: 2, flex: 1,flexDirection: 'row', justifyContent: 'space-around',}}>
+               <View style={{flex: 1,marginHorizontal: 16, marginBottom: 2, flexDirection: 'row', justifyContent: 'space-around'}}>
+                  {/*location*/}
+                  <Text style={{color: 'black', textAlign: 'center', fontSize: 24, top:200,
+                   fontWeight: 'bold', flex: 1, justifyContent: 'center', alignItems: 'center'}}
+                  > Houston,
+                     <Text style={{fontSize: 18, fontWeight: '600',color: '#808080', flex: 1,
+                     justifyContent: 'center', alignItems: 'center'}}> Texas
+                     </Text>
+                  </Text>
+                  {/*weather image*/}
+                  <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flex: 1,}}>
+                     <Image source={require('../assets/images/partlycloudy.png')}
+                      style={{ width:"150%", height:"60%", top:200, left:0, right:0}}>
+
+                     </Image>
+                  </View>
+               </View>
+            </View>
          </View>
       </SafeAreaView>
     </ImageBackground>
